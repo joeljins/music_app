666f39997cff220e031aa699b502cf50519b0d58
Quang Bui <buivuphuquang0311@gmail.com>
2024-12-04 22:34:11 -0500

    user login and registration

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const router = require("./routes/router");
+const userRouter = require("./routes/user");
 
 app.use(express.static("public"));
 
 app.use("/", router);
+
+// Use the user router for authentication
+app.use("/user", userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
