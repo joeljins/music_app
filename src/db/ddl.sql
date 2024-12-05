26cb9c9901df7ece7264ee1087f71d994c8eba4d
Quang Bui <buivuphuquang0311@gmail.com>
2024-12-05 00:23:41 -0500

    Add env.json to .gitignore and stop tracking it

--- src/db/ddl.sql
+++ src/db/ddl.sql
@@ -1,7 +1,7 @@
 -- Create the USERS table
 CREATE TABLE USERS (
     username VARCHAR(30) PRIMARY KEY,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     preferred_name VARCHAR(30),
     email VARCHAR(50) UNIQUE NOT NULL
 );
