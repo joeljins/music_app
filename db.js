b5450bbc237f4755ac38f62fe70c1b22a55604b6
Quang Bui <buivuphuquang0311@gmail.com>
2024-12-09 13:13:32 -0500

    rohan-songview

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 // Load database configuration from env.json
 const envConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, "env.json"), "utf-8"));
-const dbConfig = envConfig.db;
+const dbConfig = envConfig;
 
 // Configure the PostgreSQL pool
 const pool = new Pool(dbConfig);
