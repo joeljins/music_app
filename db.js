f81182b2b18adf43b0608e004e91d9783fb59629
JLai0 <jonathan.lai.vn@gmail.com>
2024-12-09 15:01:40 -0500

    fix

--- db.js
+++ db.js
@@ -4,9 +4,17 @@ const path = require("path");
 
 // Load database configuration from env.json
 const envConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, "env.json"), "utf-8"));
-const dbConfig = envConfig;
 
-// Configure the PostgreSQL pool
+// Map env.json keys
+const dbConfig = {
+  host: envConfig.PG_HOST,
+  port: envConfig.PG_PORT,
+  user: envConfig.PG_USER,
+  password: envConfig.PG_PASSWORD,
+  database: envConfig.PG_DATABASE,
+};
+
+// Configure the pool
 const pool = new Pool(dbConfig);
 
 module.exports = pool;
